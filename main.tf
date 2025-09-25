1e633e1ed4a67c9e4361b92ff7a1cf11d5de07fa
guanyuchen <guanyu.chen@linktivity.co.jp>
2025-09-25 17:02:50 +0900

    fix(main.tf): change depends_on for eck_operator_crds release to helm_release.eck_operator_crds

--- main.tf
+++ main.tf
@@ -4,20 +4,20 @@ locals {
   eck_operator_crds_chart_name = "eck-operator-crds"
 }
 
-resource "helm_release" "eck_operator" {
-  name       = var.eck_operator_release_name
-  chart      = local.eck_operator_chart_name
-  repository = local.eck_chart_repository
-  version    = var.chart_version
-  namespace  = var.eck_deploy_namespace
-}
-
 resource "helm_release" "eck_operator_crds" {
   name       = var.eck_operator_crds_release_name
   chart      = local.eck_operator_crds_chart_name
   repository = local.eck_chart_repository
   version    = var.chart_version
   namespace  = var.eck_deploy_namespace
+}
 
-  depends_on = [helm_release.eck_operator]
+resource "helm_release" "eck_operator" {
+  name       = var.eck_operator_release_name
+  chart      = local.eck_operator_chart_name
+  repository = local.eck_chart_repository
+  version    = var.chart_version
+  namespace  = var.eck_deploy_namespace
+
+  depends_on = [helm_release.eck_operator_crds]
 }
