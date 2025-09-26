f5deb6ddcb0973e8d78a4374c9503d0ac3a9d7e5
guanyuchen <guanyu.chen@linktivity.co.jp>
2025-09-26 11:06:35 +0900

    feat(modules/elasticsearch_cluster): add support for configurable node sets with storage and resource limits. feat(modules/elasticsearch_cluster): add support for global configuration to be able to configure global settings for all node sets.

--- modules/elasticsearch_cluster/main.tf
+++ modules/elasticsearch_cluster/main.tf
@@ -1,9 +1,9 @@
 resource "kubernetes_manifest" "es_cluster" {
-  manifest = yamldecode(templatefile("${path.module}/templates/es_cluster.yaml.tftpl",
-    {
-      es_cluster_name = var.es_cluster_name
-      es_version      = var.es_version
-      namespace       = var.namespace
-    }
-  ))
+  manifest = yamldecode(templatefile("${path.module}/templates/es_cluster.yaml.tftpl", {
+    es_cluster_name = var.es_cluster_name
+    es_version      = var.es_version
+    namespace       = var.namespace
+    node_sets       = var.node_sets
+    global_config   = var.global_config
+  }))
 }
--- modules/elasticsearch_cluster/variables.tf
+++ modules/elasticsearch_cluster/variables.tf
@@ -12,3 +12,41 @@ variable "namespace" {
   type        = string
   description = "The Kubernetes namespace where the Elasticsearch cluster will be deployed."
 }
+
+variable "global_config" {
+  type        = map(any)
+  description = "Global Elasticsearch configuration applied to all node sets"
+  default     = {}
+}
+
+variable "node_sets" {
+  type = list(object({
+    name    = string
+    count   = number
+    config  = optional(map(any), {})
+    storage = optional(object({
+      size         = optional(string, "1Gi")
+      storage_class = optional(string, "")
+    }), {})
+    resources = optional(object({
+      requests = optional(object({
+        memory = optional(string, "1Gi")
+        cpu    = optional(string, "500m")
+      }), {})
+      limits = optional(object({
+        memory = optional(string, "2Gi")
+        cpu    = optional(string, "1")
+      }), {})
+    }), {})
+  }))
+  description = "Configuration for Elasticsearch node sets"
+  default = [
+    {
+      name   = "default"
+      count  = 1
+      config = {
+        "node.store.allow_mmap" = false
+      }
+    }
+  ]
+}
