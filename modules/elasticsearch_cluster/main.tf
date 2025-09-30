b80cdade136882c58e0b0cf49d7a6e06c173823b
guanyuchen <guanyu.chen@linktivity.co.jp>
2025-09-30 15:06:41 +0900

    feat(elasticsearch_cluster): add es_image variable to the elasticsearch cluster manifest

--- modules/elasticsearch_cluster/main.tf
+++ modules/elasticsearch_cluster/main.tf
@@ -2,6 +2,7 @@ resource "kubernetes_manifest" "es_cluster" {
   manifest = yamldecode(templatefile("${path.module}/templates/es_cluster.yaml.tftpl", {
     es_cluster_name            = var.es_cluster_name
     es_version                 = var.es_version
+    es_image                   = var.es_image
     volume_claim_delete_policy = var.volume_claim_delete_policy
     namespace                  = var.namespace
     node_sets                  = var.node_sets
