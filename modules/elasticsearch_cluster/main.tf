e45b27ee7aed8c13336c193cd0dbdef841b04a3b
guanyuchen <guanyu.chen@linktivity.co.jp>
2025-10-01 11:54:43 +0900

    chore(kibana.ts): update kubernetes manifest dependencies to latest versions

--- modules/elasticsearch_cluster/main.tf
+++ modules/elasticsearch_cluster/main.tf
@@ -1,5 +1,26 @@
-resource "kubernetes_manifest" "es_cluster" {
-  manifest = yamldecode(templatefile("${path.module}/templates/es_cluster.yaml.tftpl", {
+# resource "kubernetes_manifest" "es_cluster" {
+#   manifest = yamldecode(templatefile("${path.module}/templates/es_cluster.yaml.tftpl", {
+#     es_cluster_name            = var.es_cluster_name
+#     es_version                 = var.es_version
+#     es_image                   = var.es_image
+#     image_pull_policy          = var.image_pull_policy
+#     volume_claim_delete_policy = var.volume_claim_delete_policy
+#     namespace                  = var.namespace
+#     node_sets                  = var.node_sets
+#     global_config              = var.global_config
+#     secure_settings            = var.secure_settings
+#     update_strategy            = var.update_strategy
+#     security_context           = var.security_context
+#   }))
+#
+#   field_manager {
+#     name            = "terraform"
+#     force_conflicts = true
+#   }
+# }
+
+resource "kubectl_manifest" "es_cluster" {
+  yaml_body = templatefile("${path.module}/templates/es_cluster.yaml.tftpl", {
     es_cluster_name            = var.es_cluster_name
     es_version                 = var.es_version
     es_image                   = var.es_image
@@ -11,10 +32,5 @@ resource "kubernetes_manifest" "es_cluster" {
     secure_settings            = var.secure_settings
     update_strategy            = var.update_strategy
     security_context           = var.security_context
-  }))
-
-  field_manager {
-    name            = "terraform"
-    force_conflicts = true
-  }
+  })
 }
